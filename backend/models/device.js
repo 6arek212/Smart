044e6f20c030b0bc485c7ad0e75244b7f1b437e3
6arek212 <tarik.id.10@hotmail.com>
2020-04-25 18:51:14 +0300

    asd

--- backend/models/device.js
+++ backend/models/device.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const DeviceSchema = mongoose.Schema({
   company: { type: mongoose.Schema.Types.ObjectId, ref: "Company", required: true },
-  model: { type: String, required: true, unique: true, index: true, createIndexes: true, match: /^[a-zA-Z0-9_.-]*$/ },
+  model: { type: String, required: true, unique: true, index: true, createIndexes: true, match: /^[a-zA-Z\s]*$/ },
   released: { type: Date, required: true },
   numOfRequests: { type: Number, default: 0 },
   numOfRepairs: { type: Number, default: 0 },
