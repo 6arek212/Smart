1a1fb6b854ba1a973d0398b2d874131c2f4234fe
6arek212 <tarik.id.10@hotmail.com>
2020-04-29 17:34:05 +0300

    v2

--- backend/models/message.js
+++ backend/models/message.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 
 let messageSchema = mongoose.Schema({
-  text: { type: String, required: true, match: /^([a-z\u0590-\u05fe\u0621-\u064A]+\s)*[a-z\u0590-\u05fe\u0621-\u064A]+$/ },
+  text: { type: String, required: true },
 },
   {
     timestamps: true
