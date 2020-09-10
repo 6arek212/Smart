83cc31275c810ea37aa705211b37688948278cbc
6arek212 <tarik.id.10@hotmail.com>
2020-09-10 04:08:39 +0300

    asd

--- backend/controllers/pdf.js
+++ backend/controllers/pdf.js
@@ -97,7 +97,7 @@ const getPdf = async (receipt, name) => {
   const fileName = html + name
   const content = await compile('recipt', receipt)
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.setContent(content)
   await page.pdf({
